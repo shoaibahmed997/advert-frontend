6425599ad12a1eae2e926e28adbefc1cb82573b3
Shoaib <falakshabir123@gmail.com>
2022-06-15 00:56:07 +0530

    movable items in hero section

--- src/helper/Layout.jsx
+++ src/helper/Layout.jsx
@@ -2,19 +2,32 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
 import {motion } from "framer-motion"
+
 const Layout = () => {
+  const variants = {
+    initial  : {scale:0,opacity:0},
+    animate : {scale:1,opacity:1},
+    exit : {x:200,scale:0,opacity:0},
+    transition : {type:"spring",duration:1.5}
+  }
+  const variants2 = {
+    initial  : {scale:0.8,x:-100,opacity:0},
+    animate : {scale:1,x:0,opacity:1},
+    exit : {scale:0.8,x:100,opacity:0},
+    transition : {type:"spring",duration:1.5}
+  }
+
+
   return (
     <div>
       <Header />
 
     <motion.div
-    // initial={{scale:0.8,x:-500,opacity:0}}
-    // animate={{scale:1,x:0,opacity:1}}
-    // exit={{scale:0.8,x:500,opacity:0}}
-    initial={{scale:0,opacity:0}}
-    animate={{scale:1,opacity:1}}
-    exit={{x:200,scale:2,opacity:0}}
-    transition={{type:"spring",duration:1}}
+    variants={variants}
+    initial="initial"
+    animate="animate"
+    exit="exit"
+    transition="transition"
     >
 
       <Outlet />
