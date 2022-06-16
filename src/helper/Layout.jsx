7fd960a95782913aa322c03e362e4d3630acdfcd
Shoaib <falakshabir123@gmail.com>
2022-06-16 13:36:24 +0530

    animation and user Page
    animation bug is fixed and user page is almost done

--- src/helper/Layout.jsx
+++ src/helper/Layout.jsx
@@ -3,19 +3,16 @@ import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
 import {motion } from "framer-motion"
 const Layout = () => {
+  const variant = {
+    initial:{scale:0,opacity:0},
+    animate:{scale:1,opacity:1},
+    exit:{x:200,scale:2,opacity:0},
+  }
   return (
     <div>
       <Header />
 
-    <motion.div
-    // initial={{scale:0.8,x:-500,opacity:0}}
-    // animate={{scale:1,x:0,opacity:1}}
-    // exit={{scale:0.8,x:500,opacity:0}}
-    initial={{scale:0,opacity:0}}
-    animate={{scale:1,opacity:1}}
-    exit={{x:200,scale:2,opacity:0}}
-    transition={{type:"spring",duration:1}}
-    >
+    <motion.div variants={variant} initial="initial" animate="animate" exit="exit" transition={{type:"spring",duration:.50}} >
 
       <Outlet />
     </motion.div>
--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,24 +1,85 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import { Tab,Row,Nav, Col,Button, Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import Hero from '../components/Hero'
 
 const UserPage = () => {
-    const [roll, setroll] = React.useState(false);
+    const [hasdata,sethasdata] = React.useState(false)
+    const [selectAnimation,setSelectAnimation]  = React.useState(1)
+    const variant = {
+        initial:{scale:0,opacity:0},
+        animate:{scale:[0,1,4],opacity:[0,1,0]},
+      }
+    const variant2 = {
+        initial:{scale:0,x:-200,opacity:0},
+        animate:{scale:[1,1,0.8,1],x:[-200,0,400],opacity:[0,1,0]},
+      }
   return (
     <div>
-        <motion.h1
-        animate={{
-            y:roll? 250 :0,
-            rotateX:roll ? -360:0,
-            x : roll? 1500:0,
-            scale: roll ? 3 :1,
-         }}
-         exit={{opacity:0}}
-         transition={{type:"tween",duration:2}}
-        onTap={(e,i)=>{
-            setroll(!roll)
-        }}
-        
-        >Hello User</motion.h1>
+        <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+  <Row>
+    <Col sm={3}>
+      <Nav variant="pills" className="flex-column">
+        <Nav.Item>
+          <Nav.Link eventKey="first">Posts</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="second">User</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="third">Animation Settings</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </Col>
+    <Col sm={9}>
+      <Tab.Content>
+        <Tab.Pane eventKey="first">
+        <div>
+            all post by you
+            <div>
+                {hasdata ? <Hero /> : <h1>No ad posted by you! <br />Post one <Link to='/create-post'>now</Link></h1>}
+            </div>
+        </div>
+        </Tab.Pane>
+        <Tab.Pane eventKey="second">
+          <div className='flex flex-col gap-4 mt-4 w-96'>
+              <Button variant="primary">Change Password</Button>
+              <Button variant="danger">Delete Account</Button>
+          </div>
+        </Tab.Pane>
+        <Tab.Pane eventKey="third">
+            <div>
+                <h1>Animation Style </h1>
+                <div className='flex gap-4 p-2'>
+                    <div onClick={()=>setSelectAnimation(1)} className={selectAnimation===1? 'border-2 border-blue-600' :""} >
+                        <Card>
+                            <div className='flex justify-center items-center w-full bg-orange-500 h-60'>
+                                    <motion.div variants={variant} initial="initial" animate="animate" exit="exit" transition={{type:"spring",duration:2,repeat:Infinity}}
+                                    className='w-20 h-20 bg-white rounded-lg'></motion.div>
+                            </div>
+                            <Card.Title>Style 1</Card.Title>
+                            <Card.Text>The page grows to the fullest and new page arises out of thin air</Card.Text>
+                        </Card>
+                    </div>
+                    <div onClick={()=>setSelectAnimation(2)} className={selectAnimation===2? 'border-2 border-blue-600' :""}>
+                        <Card>
+                            <div className='flex justify-center items-center w-full bg-orange-500 h-60'>
+                                    <motion.div variants={variant2} initial="initial" animate="animate" exit="exit" transition={{type:"spring",duration:2,repeat:Infinity}}
+                                    className='w-20 h-20 bg-white rounded-lg'></motion.div>
+                            </div>
+                            <Card.Title>Style 2</Card.Title>
+                            <Card.Text>The page shrinks a little and leaves to the left while new page comes from right</Card.Text>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+
+        </Tab.Pane>
+      </Tab.Content>
+    </Col>
+  </Row>
+</Tab.Container>
     </div>
 
   )
